13cd115beba92c45786a3a75e9ebedda29ffcd9a
uhi22 <uhi@hnng.de>
2024-10-04 15:50:41 +0200

    CAN: automatic bus-off recovery. No automatic retransmission.

--- stm32-tft/Core/Src/main.c
+++ stm32-tft/Core/Src/main.c
@@ -283,9 +283,9 @@ static void MX_CAN_Init(void)
   hcan.Init.TimeSeg1 = CAN_BS1_3TQ;
   hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan.Init.TimeTriggeredMode = DISABLE;
-  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoBusOff = ENABLE;
   hcan.Init.AutoWakeUp = DISABLE;
-  hcan.Init.AutoRetransmission = ENABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
   hcan.Init.ReceiveFifoLocked = DISABLE;
   hcan.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan) != HAL_OK)
